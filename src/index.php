ff028054f80643ef9f847e60020d9ab21c45b74e
Matheus <matheusalexandre.sena99@gmail.com>
2019-11-06 16:50:56 -0200

    Cors headers

--- src/index.php
+++ src/index.php
@@ -4,6 +4,7 @@
  *  Headers to enable cors and UTF-8 
  */
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
 header ('Content-type: text/html; charset=UTF-8');
 
 require_once './SendMail.php';
